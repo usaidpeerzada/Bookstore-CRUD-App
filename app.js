106ef1b4937374367809ffed7914a60ccf8828ba
Usaid <46266811+usaidpeerzada@users.noreply.github.com>
2020-07-10 00:13:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
-var mongoDB = 'mongodb://127.0.0.1/bookstore-db';
-// var mongoDB = 'mongodb+srv://admin-stark:stark123@cluster0-1k6in.mongodb.net/test?retryWrites=true&w=majority';
+var mongoDB = 'add your address';
+
 mongoose.connect(mongoDB, { useNewUrlParser: true,  useUnifiedTopology: true, useFindAndModify: false });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
